688c7f62960f86e3ea213c04ea32480182faf73a
Dasajton <david.sajitz.94@gmail.com>
2024-02-06 20:25:21 +0100

    workout exercises select-container

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,31 @@
 import { exercises } from "../data/exercises.js";
 
+function createExerciseSelectComponent(muscleGroup) {
+  return /*html*/ `
+      <div>
+        <label class="exercise-label">${muscleGroup}:</label>
+        <select name="exercises" id="exercises-select-${muscleGroup.toLowerCase()}">
+         ${exercises[muscleGroup]
+           .map((exercise) => {
+             return /*html*/ `
+          <option>${exercise}</option>
+          `;
+           })
+           .join("")}
+        </select>
+        <input type="number" id="exercises-select-${muscleGroup.toLowerCase()}-sets" placeholder="Sets">
+        <button data-select-id="exercises-select-${muscleGroup.toLowerCase()}">Add</button>
+      </div>
+    `;
+}
+
 const PageHome = () => {
   return /*html*/ `
       <div>
-          <p>Welcome to this Website</p>
+        <div class="select-container">
+          ${Object.keys(exercises).map(createExerciseSelectComponent).join("")}
+        </div>
       </div>
-      `;
+    `;
 };
-
 export default PageHome;
