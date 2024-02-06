41e0c42484b99fafb896ea93591e89e03d9113dd
Dasajton <david.sajitz.94@gmail.com>
2024-02-06 21:25:13 +0100

    div was unnecessary

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,8 @@ import { SelectExercises } from "../components/SelectedExercises.js";
 
 const PageHome = () => {
   return /*html*/ `
-    <div>
       ${SelectExercises()}
       <ul id="workout-display"></ul>
-    </div>
   `;
 };
 
